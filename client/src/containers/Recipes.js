b97117ddaf83c23404967e14675532a6e4402189
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-07-24 21:40:37 +0300

    add recipe search

--- client/src/containers/Recipes.js
+++ client/src/containers/Recipes.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Item, Grid, Segment } from 'semantic-ui-react';
+import { Item, Grid, Segment, Input } from 'semantic-ui-react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import Search from '../components/Search';
 import AddRecipeButton from '../components/AddRecipeButton';
 import RecipeItem from '../components/RecipeItem';
 import EmptyRecipes from '../components/EmptyRecipes';
@@ -31,6 +30,63 @@ class Recipes extends React.Component {
         deleteRecipe: PropTypes.func.isRequired,
     }
 
+    constructor() {
+        super();
+
+        this.state = {
+            recipes: [],
+            search: '',
+        };
+
+        this.search = this.search.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            search: this.props.recipes,
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.recipes !== this.props.recipes) {
+            this.setState({
+                recipes: this.props.recipes,
+            });
+        }
+
+        if (prevState.search !== this.state.search) {
+            this.filterRecipes(this.state.search, this.props.recipes);
+        }
+    }
+
+    search(event) {
+        this.setState({
+            search: event.target.value,
+        });
+    }
+
+    filterRecipes(searchString, recipes) {
+        if (this.state.search.length === 0) {
+            this.setState({
+                recipes,
+            });
+
+            return;
+        }
+
+        const filteredRecipes = recipes.filter((recipe) => {
+            if (recipe.title.includes(searchString) || recipe.description.includes(searchString)) {
+                return true;
+            }
+
+            return false;
+        });
+
+        this.setState({
+            recipes: filteredRecipes,
+        });
+    }
+
     render() {
         if (this.props.recipes.length === 0) {
             return <EmptyRecipes openRecipeCreate={this.props.openRecipeCreate} />;
@@ -45,12 +101,17 @@ class Recipes extends React.Component {
                                 <AddRecipeButton onClick={this.props.openRecipeCreate} />
                             </Segment>
                             <Segment textAlign="right">
-                                <Search />
+                                <Input
+                                    icon='search'
+                                    placeholder='Search...'
+                                    value={this.state.search}
+                                    onChange={this.search}
+                                />
                             </Segment>
                         </Segment.Group>
                         <Segment>
                             <Item.Group>
-                                {this.props.recipes.map(recipe => (
+                                {this.state.recipes.map(recipe => (
                                     <RecipeItem
                                         onClick={() => this.props.openRecipe(recipe._id)}
                                         delete={() => this.props.deleteRecipe(recipe._id)}
